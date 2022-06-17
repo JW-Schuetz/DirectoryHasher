3ac7ccf04d8369ed5847292b9eec1ec9678666fd
JW-Schuetz <jwschuetz1954@icloud.com>
2022-06-17 15:25:04 +0200

    Actualization

--- Directory.cpp
+++ Directory.cpp
@@ -16,11 +16,11 @@ Directory::Directory( path p )
 	cout << endl << "Anzahl Dateien: " << count << endl;
 }
 
-int Directory::crawler( path p )
+int Directory::crawler( path pstartPath )
 {
 	int count = 0;
 
-	for( directory_iterator next( p ), end; next != end; ++next )
+	for( directory_iterator next( pstartPath ), end; next != end; ++next )
 	{
 		directory_entry d = *next;
 
@@ -34,34 +34,39 @@ int Directory::crawler( path p )
 
 			// provide argument for calling certutil
 			wstring wp = d.path();
-			string p = string(wp.begin(), wp.end());
+			string p = string( wp.begin(), wp.end() );
 			string cmd = string( "certutil -hashfile " );
 			string lim = string( "\"" );
-			string arg = string( cmd +  lim + p + lim + " SHA512" );
+			string arg = string( cmd + lim + p + lim + " SHA512" );
 
-			// call certutil
-			string res = systemCall( arg.c_str() );
-
-			// process output of certutil
-			string delimiter = string( "CertUtil: -hashfile-Befehl wurde erfolgreich ausgeführt." );
-
-			int ndx = res.find( delimiter );
-			if( ndx != string::npos )
+			// call certutil if file size not zero
+			string res = "";
+			if( file_size( p ) )
 			{
-				// am Ende: delimiter entfernen
-				string token = res.substr( 0, ndx );
+				res = systemCall( arg.c_str() );
 
-				// am Anfang: SHA512-Hash entfernen
-				ndx = token.find( p );
-				token = token.substr( ndx, token.length() );
+				// process output of certutil
+				string delimiter = string( "CertUtil: -hashfile-Befehl wurde erfolgreich ausgeführt." );
 
-				// in der Mitte: NL durch " " ersetzen (und am Ende durch "")
-				token = token.replace( token.find( "\n" ), 1, " " );
-				token = token.replace( token.find( "\n" ), 1, "" );
+				int ndx = res.find( delimiter );
+				if( ndx != string::npos )
+				{
+					// am Ende: delimiter entfernen
+					string token = res.substr( 0, ndx );
 
-				cout << token << endl;
+					// am Anfang: SHA512-Hash entfernen
+					ndx = token.find( p );
+					token = token.substr( ndx, token.length() );
+
+					// in der Mitte: NL durch " " ersetzen (und am Ende durch "")
+					token = token.replace( token.find( "\n" ), 1, " " );
+					token = token.replace( token.find( "\n" ), 1, "" );
+
+					cout << token << endl;
+				}
+				else cout << "certutil scheitert bei: " + p << endl;
 			}
-			else cout << "certutil scheitert bei: " + p << endl;
+			else cout << p + ": leere Datei" << endl;
 		}
 	}
 
